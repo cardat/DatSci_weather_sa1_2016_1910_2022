cdc306c50df2aaad0ea79c7aac7161a5bf8d5b4d
Ivan Hanigan <ivan.hanigan@gmail.com>
2023-10-27 12:23:37 +0800

    add change to TERRA extract and test TERN

--- main.R
+++ main.R
@@ -11,25 +11,25 @@
 
 #### functions ####
 library(raster)
+library(terra)
 library(ncdf4)
-library(exactextractr)
 library(sf)
 library(data.table)
 
 #### input ####
-infile <- "https://dapds00.nci.org.au/thredds/dodsC/zv2/agcd/v1-0-1/tmax/mean/r005/01day/agcd_v1-0-1_tmax_mean_r005_daily_2022.nc"
-#infile <- "https://dap.tern.org.au/thredds/dodsC/Historical_Extreme_Weather_Events_Australia/AWAP_Year/uncompressed/Un_AWAP_2020.nc"
+infile <- "https://dapds00.nci.org.au/thredds/dodsC/zv2/agcd/v1-0-1/tmax/mean/r005/01day/agcd_v1-0-1_tmax_mean_r005_daily_2020.nc"
 
 #### variables ####
 r_nc <- ncdf4::nc_open(infile)
 r_nc
 varlist <- names(r_nc[['var']])
+varlist
 # exclude lon, lat, time and crs
 varlist <- varlist[5:length(varlist)]
 varlist
 ## there is only one variable in this climate grids repository 
-#var_i <- "Temp_max"
 var_i <- "tmax"
+
 
 #### load ABS SA1 for TAS ####
 indir_sa1 <- "data_provided/ABS_Census_2016/abs_sa1_2016_data_provided"
@@ -38,48 +38,53 @@ infile_sa1 <- "SA1_2016_TAS.shp"
 sa1 <- st_read(file.path(indir_sa1, infile_sa1))
 
 #### load and extract for study period ####
-
-study_period <- list(mindate="2022-01-01", maxdate ="2022-01-31")
-
+sa1_out <- list()
+for(yy in 2001:2020){
+  ## yy = 2001
+  study_period <- list(mindate=paste0(yy,"-01-01"), maxdate =paste0(yy,"-01-10"))
+  
 ## raster data for each var
-print(var_i)
-b <- raster::brick(infile)#, varname = var_i)
-##b
-b2 <- b[[which(getZ(b) >= as.Date(study_period[["mindate"]]) & getZ(b) <= as.Date(study_period[["maxdate"]]))]]
-b_sa1 <- exact_extract(brick(b2), sa1, progress = FALSE) 
-## ignoring warning about CRS for now
-b_sa1 <- rbindlist(b_sa1, idcol = "rowid")
-##b_sa1
-## make long format from wide
-b_sa1_long <- melt(b_sa1, id.var = c("rowid", "coverage_fraction"))
-## check a day
-## b_sa1_long[variable == "X2020.01.19"]
-b_sa1_out <- b_sa1_long[,
-                        .(value = sum(value * coverage_fraction, na.rm = T) / sum(coverage_fraction, na.rm = T)),
-                        by = .(rowid, variable)
-]
-## checks for one day
-# b_sa1_out2 <- b_sa1_out[variable == "X2020.01.19"]
-# sa1_map <- cbind(sa1, b_sa1_out2)
-# plot(sa1_map["value"])
-names(b_sa1_out) <- c("rowid", "date", "value")
-b_sa1_out$variable <- var_i
-outdat <- b_sa1_out
+# print(var_i)
+  b <- raster::brick(gsub("agcd_v1-0-1_tmax_mean_r005_daily_2020.nc", 
+                          paste0("agcd_v1-0-1_tmax_mean_r005_daily_",yy,".nc"), infile), 
+                     varname = var_i)
+  # b
+  # plot(b)
+  b2 <- b[[which(getZ(b) >= as.Date(study_period[["mindate"]]) & getZ(b) <= as.Date(study_period[["maxdate"]]))]]
+  
+  b2 <-rast(brick(b2))
+  b_sa1 <- terra::extract(b2, sa1, mean)
+  out <- cbind(st_drop_geometry(sa1[,1]), b_sa1)
+  setDT(out)
+  
+  out2 <- melt(out, id.vars = c(names(out)[1], "ID"))
+  out2$measure <- var_i
+  sa1_out[[yy]] <- out2
+}
 
+sa1_out2 <- rbindlist(sa1_out)
+sa1_out2
+## check a day
+sa1_out2[substr(sa1_out2$variable, 6, 11) == ".01.09"
+         & SA1_MAIN16 == 60101100101 ]
+
+
+outdat <- sa1_out2
+str(outdat)
 
 ## tidy up the dates
-outdat$date <- gsub("X","",outdat$date)
+outdat$date <- gsub("X","",outdat$variable)
 outdat$date <- as.Date(gsub("\\.","-",outdat$date))
-
-outdat_wide <- dcast(outdat, rowid + date ~ variable, value.var = "value")
+outdat$variable <- NULL
+outdat_wide <- dcast(outdat, SA1_MAIN16 + ID + date ~ measure, value.var = "value")
 
 ## get sa1 codes
 sa1_df <- st_drop_geometry(sa1)
-sa1_df$rowid <- 1:nrow(sa1_df)
+sa1_df$ID <- 1:nrow(sa1_df)
 setDT(sa1_df)
 
-outdat_wide2 <- merge(sa1_df[,.(rowid, SA1_7DIG16, SA4_NAME16, GCC_NAME16)], outdat_wide, by = "rowid")
-unique(outdat_wide2$SA4_NAME16)
+outdat_wide2 <- merge(sa1_df[,.(SA1_MAIN16, SA1_7DIG16 , ID)], outdat_wide, by = "ID")
+unique(outdat_wide2$SA1_7DIG16 )
 
 #### show a time series plot for a single SA1 in launceston ####
 
@@ -93,23 +98,25 @@ with(sa1_toplot, plot(date, tmax, type = "b", ylim = c(0,40)))
 title(paste0("time series tmax for sa1", sa1_todo))
 dev.off()
 
-#### show a map of the 22nd ####
+#### show a map of the 2nd ####
 
-sa1_tomap <- outdat_wide2[date == as.Date("2022-01-22")]
+sa1_tomap <- outdat_wide2[date == as.Date("2001-01-02")]
 sa1_map <- cbind(sa1, sa1_tomap)
 
-png("figures_and_tables/do_map_abs_sa1_tmax_tas_20220122.png", width = 700, height = 700)
+png("figures_and_tables/do_map_abs_sa1_tmax_tas_20010102.png", width = 700, height = 700)
 plot(sa1_map["tmax"])
-legend("bottomright", legend = "22 Jan 2022")
+legend("bottomright", legend = "2 Jan 2001")
 dev.off()
 
 
 #### show the whole map ####
 
 var_i = "tmax"
-b <- raster::brick(infile, varname = var_i)
-b2 <- b[[which(getZ(b) >= as.Date("2022-01-22") & getZ(b) < as.Date("2022-01-23"))]]
-png("figures_and_tables/do_map_abs_sa1_tmax_pred_national_20220122.png", width = 700, height = 700)
+b <- raster::brick(gsub("agcd_v1-0-1_tmax_mean_r005_daily_2020.nc", 
+                        paste0("agcd_v1-0-1_tmax_mean_r005_daily_",yy,".nc"), infile), 
+                   varname = var_i)
+b2 <- b[[which(getZ(b) >= as.Date("2001-01-02") & getZ(b) < as.Date("2001-01-03"))]]
+png("figures_and_tables/do_map_abs_sa1_tmax_pred_national_20010102.png", width = 700, height = 700)
 plot(b2)
 dev.off()
 
@@ -118,7 +125,7 @@ nc_close(r_nc)
 
 ## store grid and shapefile for GIS display
 dir.create("data_derived")
-writeRaster(b2, "data_derived/tmax_20220122.tif")
-st_write(sa1_map, "data_derived/tmax_20220122_abs_sa1_tas.gpkg", driver = "gpkg")
+writeRaster(b2, "data_derived/tmax_20010102.tif")
+st_write(sa1_map, "data_derived/tmax_20010102_abs_sa1_tas.gpkg", driver = "gpkg")
 ## open these in QGIS and produce a map with a scale bar and legend
 
